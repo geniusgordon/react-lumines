494c6e9671ede4cb834649dca7cb8f579318ad44
Gordon <geniusgordon@gmail.com>
2018-01-22 23:22:46 +0800

    react-ga

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Route } from 'react-router';
 
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import ReactGA from 'react-ga';
+
 import App from './App';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import withTracker from './withTracker';
+
 import './index.css';
+
+ReactGA.initialize('UA-68526607-8');
+ReactGA.pageview(window.location.pathname);
 
 const store = createStore(reducer);
 
@@ -27,7 +34,7 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <Provider store={store}>
       <MemoryRouter>
-        <App />
+        <Route component={withTracker(App)} />
       </MemoryRouter>
     </Provider>
   </ApolloProvider>,
---
+++ src/withTracker.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import GoogleAnalytics from 'react-ga';
+
+GoogleAnalytics.initialize('UA-68526607-8');
+
+const withTracker = (WrappedComponent, options = {}) => {
+  const trackPage = page => {
+    GoogleAnalytics.set({
+      page,
+      ...options,
+    });
+    GoogleAnalytics.pageview(page);
+  };
+
+  const HOC = class extends Component {
+    componentDidMount() {
+      const page = this.props.location.pathname;
+      trackPage(page);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+
+  return HOC;
+};
+
+export default withTracker;
