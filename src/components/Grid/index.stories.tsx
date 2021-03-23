1118b93ae25d31193223198530f946fa2d3e58e7
Gordon Ueng <geniusgordon@gmail.com>
2021-03-23 22:02:35 +0800

    Add Grid

---
+++ src/components/Grid/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import Grid from '.';
+import SvgDecorator from '../SvgDecorator';
+
+export { Grid };
+
+export default {
+  title: 'Grid',
+  component: Grid,
+  decorators: SvgDecorator,
+} as Meta;
